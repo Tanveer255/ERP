c2a628ede029fee96b99177e195ec9811874817f
Muhammad Tanveer <tanveerofficial244@gmail.com>
2026-03-20 22:31:22 +0500

    automatic database creation (and optionally migration) at app startup when using Code

--- ERP/Program.cs
+++ ERP/Program.cs
@@ -16,7 +16,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+
+// Auto apply migrations
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ManufacturingDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
