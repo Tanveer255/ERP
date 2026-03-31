08ab107d3776590ca517fcb5fb7842f4db2c42d4
Muhammad Tanveer <tanveerofficial244@gmail.com>
2026-03-31 11:33:10 +0500

    reservationStatuses_addedinInsaleorder

---
+++ ERP/Migrations/20260331063239_reservationStatuses_addedinInsaleorder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Migrations
+{
+    /// <inheritdoc />
+    public partial class reservationStatuses_addedinInsaleorder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ReservationStatus",
+                table: "SalesOrders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReservationStatus",
+                table: "SalesOrders");
+        }
+    }
+}
