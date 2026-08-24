7ca964ce4be8c15c936ea7d1b07caaf87e194dff
Muhammad Tanveer <tanveerofficial244@gmail.com>
2026-08-24 12:27:06 +0500

    complete production logic added

--- ERP/Controllers/Order/ProductionController.cs
+++ ERP/Controllers/Order/ProductionController.cs
@@ -279,7 +279,7 @@ public class ProductionController : ControllerBase
 
     #region Complete Production
     /// <summary>
-    /// Complete production added: consume reserved stock, produce finished goods, update order status to Completed, handle concurrency with transaction
+    /// Complete production added logic: consume reserved stock, produce finished goods, update order status to Completed, handle concurrency with transaction
     /// </summary>
     /// <param name="orderId"></param>
     /// <returns></returns>
