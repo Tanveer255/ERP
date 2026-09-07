dbdabc182d728a30eae45fb246a860fefacbb9cb
Muhammad Tanveer <tanveerofficial244@gmail.com>
2026-09-07 20:42:27 +0500

    get production on orders

--- ERP/Controllers/Order/ProductionController.cs
+++ ERP/Controllers/Order/ProductionController.cs
@@ -41,7 +41,7 @@ public class ProductionController : ControllerBase
         _productStockService = productStockService;
     }
     /// <summary>
-    /// get production on orders: return list of production orders with details, sorted by planned start date descending
+    /// g: return list of production orders with details, sorted by planned start date descending
     /// </summary>
     /// <returns></returns>
     [HttpGet]
