8d19aa81504333a074c2fe480513be52f8e450de
Muhammad Tanveer <tanveerofficial244@gmail.com>
2026-09-08 20:58:47 +0500

    private readonly ProductStockService _productStockService;  added

--- ERP/Controllers/Order/ProductionController.cs
+++ ERP/Controllers/Order/ProductionController.cs
@@ -23,7 +23,6 @@ public class ProductionController : ControllerBase
     private readonly PurchaseOrderService _purchaseOrderService;
     private readonly ProductionOperationService _productionOperationService;
     private readonly MrpService _mrpService;
-    private readonly ProductStockService _productStockService;
 
     public ProductionController(ManufacturingDbContext context,
         ProductionOrderService productionOrderService,
