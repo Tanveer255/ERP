82ef598aed8ee8eb04880586bafaae2c95224f19
Muhammad Tanveer <tanveerofficial244@gmail.com>
2026-09-14 16:56:02 +0500

    private readonly MrpService _mrpService; added in production controller

--- ERP/Controllers/Order/ProductionController.cs
+++ ERP/Controllers/Order/ProductionController.cs
@@ -22,6 +22,7 @@ public class ProductionController : ControllerBase
     private readonly ProductionOrderService _productionOrderService;
     private readonly PurchaseOrderService _purchaseOrderService;
     private readonly ProductionOperationService _productionOperationService;
+    private readonly MrpService _mrpService;
     private readonly ProductStockService _productStockService;
 
     public ProductionController(ManufacturingDbContext context,
